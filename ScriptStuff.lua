cfa240e9f7eafac4be53de01e933adfa77d0ac20
Zaphick3L <99723503+Zaphick3L@users.noreply.github.com>
2022-03-20 01:00:41 +0100

    Update ScriptStuff.lua

--- ScriptStuff.lua
+++ ScriptStuff.lua
@@ -1,24 +1,11 @@
-if not workspace[game.Players.LocalPlayer.Name]:FindFirstChild("Raw") then
-			local sound = Instance.new("Sound", game.StarterGui)
-		sound.SoundId = "rbxassetid://8426701399"
-		sound.Volume = 3
-		sound:Play()
-		game.StarterGui:SetCore("SendNotification", {
-			Title = "Unim Hub",
-			Text = "Not Reanimated!"
-		})
-		wait(2)
-		sound:Destroy()
-		return
-end
-
+-- i keeped it off because i didnt wanna copy paste that for years
 if not workspace[game.Players.LocalPlayer.Name]:FindFirstChild(TheHatLol) then
 			local sound = Instance.new("Sound", game.StarterGui)
 		sound.SoundId = "rbxassetid://8426701399"
 		sound.Volume = 3
 		sound:Play()
 		game.StarterGui:SetCore("SendNotification", {
-			Title = "Unim Hub",
+			Title = "Spirit Hub",
 			Text = "Missing Hat: ".. TheHatLol
 		})
 		wait(2)
@@ -32,27 +19,10 @@ if not workspace[game.Players.LocalPlayer.Name]:FindFirstChild(TheHatLol2) then
 		sound.Volume = 3
 		sound:Play()
 		game.StarterGui:SetCore("SendNotification", {
-			Title = "Unim Hub",
+			Title = "Spirit Hub",
 			Text = "Missing Hat: ".. TheHatLol2
 		})
 		wait(2)
 		sound:Destroy()
 		return
 end
-
-if workspace[game.Players.LocalPlayer.Name]:FindFirstChild("NoYouWillNotRunScriptAgain.") then
-		local sound = Instance.new("Sound", game.StarterGui)
-		sound.SoundId = "rbxassetid://8426701399"
-		sound.Volume = 3
-		sound:Play()
-		game.StarterGui:SetCore("SendNotification", {
-			Title = "Unim Hub",
-			Text = "Script is already Running! Reset to change a script."
-		})
-		wait(2)
-		sound:Destroy()
-		return
-end
-
-local fufufuff = Instance.new("Part", workspace[game.Players.LocalPlayer.Name])
-fufufuff.Anchored = true;fufufuff.Transparency = 1;fufufuff.CanCollide = false;fufufuff.Name = "NoYouWillNotRunScriptAgain."
